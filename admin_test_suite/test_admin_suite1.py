6abe3e0d0abb150a5f5a8bdba84e135ecf3f3640
msmfathih <msmfathih@users.noreply.github.com>
2020-07-05 22:17:41 +0400

    TestSuit1 and 2 Updated

--- admin_test_suite/test_admin_suite1.py
+++ admin_test_suite/test_admin_suite1.py
@@ -4,12 +4,8 @@ import pytest
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-import allure
-"""
-RUN TEST SUITE IN TERMINAL
-py.test -v -s C:\Users\fathih\PycharmProjects\PHPTravel\admin_test_suite
 
-"""
+
 
 class TestExecution():
 
@@ -22,7 +18,7 @@ class TestExecution():
         print("Maximized browser")
 
     @pytest.mark.run(order=2)
-    def test_validlogin(self):
+    def test_varifyPageURL(self):
         driver.get("http://rentvehicles.multicompetition.com/")
 
         try:
@@ -35,7 +31,7 @@ class TestExecution():
 
 
     @pytest.mark.run(order=3)
-    def test_invalidlogin(self):
+    def test_validlogin(self):
         enterEmail = driver.find_element_by_id("email")
         enterEmail.send_keys("admin@gmail.com")
         time.sleep(3)
@@ -53,6 +49,7 @@ class TestExecution():
 
     @pytest.mark.run(order=4)
     @pytest.mark.timeout(10)
+    @pytest.mark.skip(reason="not needed as per the flow")
     def test_vahiclePage_section(self):
 
         clickVehicleDropDown = driver.find_element(By.XPATH, "//p[contains(text(),'Vehicles')]")
@@ -89,8 +86,6 @@ class TestExecution():
         performSubmitBtn = driver.find_element_by_xpath("//button[@class='btn btn-primary btn-submit']")
         performSubmitBtn.click()
         time.sleep(3)
-
-
 
 
 
--- admin_test_suite/test_admin_suite2.py
+++ admin_test_suite/test_admin_suite2.py
@@ -4,22 +4,53 @@ import pytest
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-import allure
+
 
 
 class TestExecution2():
 
-    @pytest.mark.run(order=1)
+    @pytest.mark.run(order=4)
     def test_setUp(self):
         global driver
-        #driver = webdriver.Chrome(executable_path="E:\\Automation\\Webdrivers\\chromedriver\\chromedriver.exe")
+        driver = webdriver.Chrome(executable_path="E:\\Automation\\Webdrivers\\chromedriver\\chromedriver.exe")
         driver.implicitly_wait(10)
         # driver.maximize_window()
         print("Maximized browser")
 
 
+    @pytest.mark.run(order=5)
+    def test_invalidlogin(self):
+        driver.get("http://rentvehicles.multicompetition.com/")
+        enterEmail = driver.find_element_by_id("email")
+        enterEmail.send_keys("admin@gmail.com")
+        time.sleep(3)
+
+        enterPassword = driver.find_element_by_id("password")
+        enterPassword.send_keys("admin@123")
+        time.sleep(3)
+
+        while True:
+            try:
+                driver.find_element_by_id("btnLogin").click()
+            except:
+                break
+        time.sleep(2)
+
     @pytest.mark.run(order=6)
+    def test_vahiclePage_section(self):
+
+        clickVehicleDropDown = driver.find_element(By.XPATH, "//p[contains(text(),'Vehicles')]")
+        clickVehicleDropDown.click()
+        time.sleep(2)
+
+        clickVehicleOwner = driver.find_element_by_xpath("//p[contains(text(),'Vehicle Owners')]")
+        clickVehicleOwner.click()
+        time.sleep(2)
+
+
+    @pytest.mark.run(order=7)
     def test_verify_ownerName(self):
+
         assert "vehicle-owners" in driver.current_url
 
         verifyVehicleOwnerText = driver.find_element(By.XPATH, "//h1[@class='m-0 text-dark']")
@@ -39,14 +70,14 @@ class TestExecution2():
         assert verifyOwnerName.text == 'milhan'
         print("New vehicle owner name is "+str(verifyOwnerName))
 
-    @pytest.mark.run(order=7)
+    @pytest.mark.run(order=8)
     def test_phone_number(self):
         verifyPhoneNumber = driver.find_element_by_xpath("//td[contains(text(),'0528542756')]")
         assert verifyPhoneNumber.text == '0528542756'
         print("New vehicle owner phonenumber is "+str(verifyPhoneNumber))
         time.sleep(3)
 
-    @pytest.mark.run(order=8)
+    @pytest.mark.run(order=7)
     def test_verify_nic(self):
         verifynic = driver.find_element_by_xpath("//td[contains(text(),'920012214V')]")
         assert verifynic.text == '920012214V'
