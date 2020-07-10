87a6dedff25e454d8c1f79aa21ae7aeed4d4f460
msmfathih <msmfathih@users.noreply.github.com>
2020-07-10 15:28:28 +0400

    test_verify_driver.py: completed

--- driver_registration_flow/test_verify_driver.py
+++ driver_registration_flow/test_verify_driver.py
@@ -10,14 +10,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 class TestDrivers3():
 
-    @pytest.mark.run(order=1)
+    @pytest.mark.run(order=27)
     def test_setUp(self):
         global driver
         driver = webdriver.Chrome(ChromeDriverManager().install())
         driver.implicitly_wait(10)
         print("maximized browser")
 
-    @pytest.mark.run(order=2)
+
+    @pytest.mark.run(order=28)
     def test_valid_login(self):
         driver.get("http://rentvehicles.multicompetition.com/login")
         time.sleep(2)
@@ -47,7 +48,7 @@ class TestDrivers3():
         enterLoginBtn.click()
         time.sleep(3)
 
-    @pytest.mark.run(order=3)
+    @pytest.mark.run(order=29)
     def test_navigate_driver_section(self):
         driver.find_element_by_xpath("/html/body/div[1]/aside[1]/div/div[4]/div/div/nav/ul/li[2]/a").click()
         time.sleep(2)
@@ -55,30 +56,52 @@ class TestDrivers3():
         driver.find_element(By.XPATH, "//p[contains(text(),'Drivers List')]").click()
         time.sleep(2)
 
+
+    @pytest.mark.run(order=30)
     def test_driver_list_view(self):
         click_action_view = driver.find_element_by_xpath("//tr[4]//td[6]//a[1]")
         click_action_view.click()
         time.sleep(3)
 
+
+
+    @pytest.mark.run(order=31)
     def test_verify_driver_name(self):
         verify_drivername = driver.find_element_by_xpath("//label[contains(text(),'Driver Name : aadil')]")
         elementText = verify_drivername.text
         print("Text on element is " + elementText)
 
+
+
+    @pytest.mark.run(order=32)
     def test_verify_driver_email(self):
         verify_email = driver.find_element_by_xpath("//label[contains(text(),'Email : aadil@gmail1.com')]")
         elementText = verify_email.text
         print("Text on element is " + elementText)
 
+
+    @pytest.mark.run(order=33)
     def test_verify_nic_number(self):
         verify_nic = driver.find_element_by_xpath("//label[contains(text(),'NIC Number: 9200122134V')]")
         elementText = verify_nic.text
         print("Driver NIC is "+elementText)
 
+    @pytest.mark.run(order=34)
     def test_verify_phonenumber(self):
-        element = driver.find_element_by_xpath("//label[contains(text(),'Phone Number : 0528542762')]")
-        assert element.text == '0528542762'
+        verify_phoneNumber = driver.find_element_by_xpath("//label[contains(text(),'Phone Number : 0528542762')]")
+        elementText = verify_phoneNumber.text
+        print("Driver Phone number is "+elementText)
+        time.sleep(3)
 
+        driver.find_element_by_xpath("//a[@id='cancelButton']").click()
+        time.sleep(3)
+
+    @pytest.mark.run(order=35)
+    def test_logout_app(self):
+        driver.find_element_by_xpath("//p[contains(text(),'Need to logout ?')]").click()
+        time.sleep(2)
+        driver.find_element_by_xpath("//p[contains(text(),'Logout')]").click()
+        time.sleep(3)
 
 
     def test_tearDown(self):
